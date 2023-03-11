751de45d860bef53e8546e2c3c79ca73c7b8f08a
billyhjm <billyhjm@gmail.com>
2023-03-11 23:49:48 +0900

    Added sever code to run server

--- src/app.py
+++ src/app.py
@@ -16,6 +16,7 @@ db = client["NDT"]
 collection = db["NDT"]
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
 
 app.layout = html.Div([
 
